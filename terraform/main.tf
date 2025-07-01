90bac3cc5120ced5d9ae87d54ee2ffc3c2702a4e
“alexandrupopayoulend” <alexandru.popa@youlend.com>
2025-07-01 10:57:07 +0100

    Update Sandbox Email

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "controltowersquad+sandbox@proton.me"
+    AccountName               = "Sandbox-aft"
+    ManagedOrganizationalUnit = "Product"
+    SSOUserEmail              = "controltowersquad@proton.me"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "Sandbox"
 }
